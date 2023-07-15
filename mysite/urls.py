99248211356164f1acba37500d407dd9e0fea733
sakiyamamamama <l220429@mail.ryukoku.ac.jp>
2023-07-15 22:54:50 +0900

    ファイル整理

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path
 from accounts.views import index
 
+app_name = 'accounts'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
