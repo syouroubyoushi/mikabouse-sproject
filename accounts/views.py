03cc9d73e91f0683488f7d70322b9d223bf6bc59
sinogi0119 <sinogi0119@gmail.com>
2023-07-21 18:04:09 +0900

    use lib bootstrap

--- accounts/views.py
+++ accounts/views.py
@@ -2,3 +2,5 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'index.html')
+def regist(request):
+    return render(request, 'regist.html')
--- mysite/urls.py
+++ mysite/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from accounts.views import index
+from accounts.views import regist
 
 app_name = 'accounts'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
+    path('regist/', regist, name='regist'),
 ]
