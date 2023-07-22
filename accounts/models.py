4bb11e0c81994b971fe5d5d898f7842f7d0caada
sakiyamamamama <l220429@mail.ryukoku.ac.jp>
2023-07-22 18:45:14 +0900

    テキスト投稿のデータベースモデル作成

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,12 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.conf import settings
 
 class CustomUser(AbstractUser):
     groups = models.ManyToManyField('auth.Group', related_name='custom_user_groups')
     user_permissions = models.ManyToManyField('auth.Permission', related_name='custom_user_permissions')
+
+class Text(models.Model):
+    text=models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
